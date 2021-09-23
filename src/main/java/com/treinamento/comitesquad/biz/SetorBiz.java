f2f48fcda2551e88fceb0d4b8a871ca4ff0febf6
Diogo L. Menezes <dmenezes@itelinfo.com>
2021-09-23 11:10:56 -0400

    Adicionando Biz de Atleta e de Setor

--- src/main/java/com/treinamento/comitesquad/biz/SetorBiz.java
+++ src/main/java/com/treinamento/comitesquad/biz/SetorBiz.java
@@ -1,5 +1,28 @@
 package com.treinamento.comitesquad.biz;
 
-public class SetorBiz {
+import com.treinamento.comitesquad.Mensagem;
+import com.treinamento.comitesquad.entities.Setor;
+import com.treinamento.comitesquad.repositories.ComiteRepository;
 
+public class SetorBiz {
+    private Mensagem mensagens;
+    private ComiteRepository comiteRepositorio;
+    public SetorBiz(ComiteRepository comiteRepo){
+        this.comiteRepositorio = comiteRepo;
+    }
+    public boolean verificaSetor(Setor setor){
+        boolean valido= true;
+        if(setor.getId_comite()==null){
+            mensagens.mensagem.add("O id da comite informado não deve ser nulo");
+            valido=false;
+        }else if(comiteRepositorio.findById(setor.getId_comite()).isEmpty()){
+            mensagens.mensagem.add("Nenhuma comite foi encontrada com o id informado");
+            valido=false;
+        }
+        if(setor.getNome().isBlank() || setor.getNome() == null){
+            mensagens.mensagem.add("O nome informado não deve ser nulo");
+            valido=false;
+        }
+        return valido;
+    }
 }
