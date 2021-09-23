f826072088a6939c15955a63598e575d40e0fb93
Lucas D. Soares <lsoares@itelinfo.com>
2021-09-23 11:24:14 -0400

    Implementação Controller do Comite

--- src/main/java/com/treinamento/comitesquad/controller/ComiteController.java
+++ src/main/java/com/treinamento/comitesquad/controller/ComiteController.java
@@ -1,5 +1,41 @@
 package com.treinamento.comitesquad.controller;
 
-public class ComiteController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.treinamento.comitesquad.entities.Comite;
+import com.treinamento.comitesquad.repositories.ComiteRepository;
+
+@RestController
+@RequestMapping("comite")
+public class ComiteController {
+	
+	@Autowired
+	private ComiteRepository comiteRepository;
+	
+	@GetMapping("listar")
+	public List<Comite> listarComite(){
+		List<Comite> lista = comiteRepository.findAll();
+		return lista;
+	}
+	
+	  @PostMapping("incluir")
+	    public String incluirCliente(@RequestBody Comite comite) {
+	        
+	        try{
+	            comiteRepository.save(comite);
+	            comiteRepository.flush();
+	            return comite.toString();
+	        }catch(Exception e) {
+	            return e.getMessage();
+	        }
+	        
+	    }
+	  
 }
