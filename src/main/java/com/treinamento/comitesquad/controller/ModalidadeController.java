42ca4151473b6480f159a0672016d085a47ab561
Misael Lima <misaellimadossantos@hotmail.com>
2021-09-23 12:09:28 -0400

    Acrescentando o RequestMapping a Classe ModalidadeController

--- src/main/java/com/treinamento/comitesquad/controller/ModalidadeController.java
+++ src/main/java/com/treinamento/comitesquad/controller/ModalidadeController.java
@@ -7,6 +7,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.treinamento.comitesquad.Mensagem;
 import com.treinamento.comitesquad.biz.ModalidadeBiz;
@@ -14,6 +16,8 @@ import com.treinamento.comitesquad.entities.Modalidade;
 import com.treinamento.comitesquad.repositories.ConfederacaoRepository;
 import com.treinamento.comitesquad.repositories.ModalidadeRepository;
 
+@RestController
+@RequestMapping("modalidade")
 public class ModalidadeController {
 	@Autowired
 	private ModalidadeRepository modalidadeRepository;
