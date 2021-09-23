8b6318081c193eaaf5fe4a2b658d904afa00ecd2
emersonnobre <egrnobre@gmail.com>
2021-09-23 10:42:19 -0400

    Criação das entidades de Modalidade e Evento, assim como seus repositórios.

--- src/main/java/com/treinamento/comitesquad/repositories/ModalidadeRepository.java
+++ src/main/java/com/treinamento/comitesquad/repositories/ModalidadeRepository.java
@@ -1,5 +1,10 @@
 package com.treinamento.comitesquad.repositories;
 
-public interface ModalidadeRepository {
+import com.treinamento.comitesquad.entities.Modalidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ModalidadeRepository extends JpaRepository<Modalidade, Integer> {
 
 }
