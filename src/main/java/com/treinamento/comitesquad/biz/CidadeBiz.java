9b0a66ec488b796dbe239efc343c30414cdcf8cc
Marcus Vinicius R. Oliveira <moliveira@itelinfo.com>
2021-09-23 11:30:47 -0400

    CidadeController e CidadeBiz

--- src/main/java/com/treinamento/comitesquad/biz/CidadeBiz.java
+++ src/main/java/com/treinamento/comitesquad/biz/CidadeBiz.java
@@ -1,5 +1,44 @@
 package com.treinamento.comitesquad.biz;
 
+import com.treinamento.comitesquad.Mensagem;
+import com.treinamento.comitesquad.entities.Cidade;
+import com.treinamento.comitesquad.repositories.ProvinciaRepository;
+
 public class CidadeBiz {
 
+	private Mensagem msg;
+	private ProvinciaRepository provinciaRepository;
+	
+	public CidadeBiz( ProvinciaRepository provinciaRepository) {
+		this.provinciaRepository = provinciaRepository;
+		this.msg = new Mensagem();
+	}
+
+	public Boolean validar( Cidade cidade) {
+		Boolean valido = true;
+		
+		if (cidade.getNome().isEmpty()) {
+			msg.getMensagem().add("O nome não pode ser vazio");
+			valido = false;
+		}else if(cidade.getNome().length() > 255) {
+			msg.getMensagem().add("O nome não pode ser maior que 255 caracteres");
+			valido = false;
+		}
+			
+		if (provinciaRepository.findById(cidade.getId_provincia()).isEmpty()) {
+			msg.getMensagem().add(("O setor do funcionario não existe"));
+			valido = false;
+		}
+		
+		return valido;
+	}
+	
+
+	public Mensagem getMsg() {
+		return msg;
+	}
+
+	public void setMsg(Mensagem msg) {
+		this.msg = msg;
+	}
 }
--- src/main/java/com/treinamento/comitesquad/controller/CidadeController.java
+++ src/main/java/com/treinamento/comitesquad/controller/CidadeController.java
@@ -3,12 +3,18 @@ package com.treinamento.comitesquad.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.treinamento.comitesquad.Mensagem;
+import com.treinamento.comitesquad.biz.CidadeBiz;
 import com.treinamento.comitesquad.entities.Cidade;
 import com.treinamento.comitesquad.repositories.CidadeRepository;
+import com.treinamento.comitesquad.repositories.ProvinciaRepository;
 
 	
 @RestController
@@ -17,11 +23,33 @@ public class CidadeController {
 
 	@Autowired	
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired 
+	private ProvinciaRepository provinciaRepository;
 
 	@GetMapping("listar")
 	public List<Cidade> listar(){
 		List<Cidade> lista = cidadeRepository.findAll();
 		return lista;
+	}
+	
+	@PostMapping("incluir")
+	public Mensagem incluirCidade(@Validated @RequestBody Cidade cidade) {
+		
+		CidadeBiz cidadeBiz = new CidadeBiz(this.provinciaRepository);
+		try {
+			
+			if (cidadeBiz.validar(cidade)) {
+				cidadeRepository.save(cidade);
+				cidadeRepository.flush();
+				cidadeBiz.getMsg().getMensagem().add("OK");
+			}
+			
+		} catch (Exception e) {
+			cidadeBiz.getMsg().getMensagem().add("Erro ao Incluir:" + e.getMessage());
+		}
+		
+		return cidadeBiz.getMsg();
 	}
 	 
 }
