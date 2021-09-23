df8e0aeef5cfb844dc4db54de52961f4faee0718
Marcus Vinicius R. Oliveira <moliveira@itelinfo.com>
2021-09-23 10:49:47 -0400

    Controller listar

--- src/main/java/com/treinamento/comitesquad/entities/Cidade.java
+++ src/main/java/com/treinamento/comitesquad/entities/Cidade.java
@@ -5,11 +5,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import org.springframework.web.bind.annotation.RequestMapping;
+import javax.persistence.Table;
 
 @Entity
-@RequestMapping("cidade")
+@Table(name="tb_cidade")
 public class Cidade {
 
 	@Id
