bf678c57aac9018296e8a1d5be8a14be4dab0db2
Misael Lima <misaellimadossantos@hotmail.com>
2021-09-23 10:44:01 -0400

    Implementando a entidade Confederacao

--- src/main/java/com/treinamento/comitesquad/entities/Confedercao.java
+++ src/main/java/com/treinamento/comitesquad/entities/Confedercao.java
@@ -1,5 +1,46 @@
 package com.treinamento.comitesquad.entities;
 
-public class Confedercao {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
+@Entity
+@Table(name = "tb_confederacao")
+public class Confedercao {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	@NotNull
+	@Column(name = "nome")
+	private String nome;
+	@NotNull
+	@Column(name = "id_comite")
+	private Integer id_comite;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getId_comite() {
+		return id_comite;
+	}
+	public void setId_comite(Integer id_comite) {
+		this.id_comite = id_comite;
+	}
+	
+	
 }
