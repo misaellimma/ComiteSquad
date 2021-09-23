73ec84a6087649e8a92f2a0b1bb3f0fc106b5f3f
Misael Lima <misaellimadossantos@hotmail.com>
2021-09-23 11:26:06 -0400

    Implementado a Classe ConfederacaoBiz

--- src/main/java/com/treinamento/comitesquad/biz/ConfederacaoBiz.java
+++ src/main/java/com/treinamento/comitesquad/biz/ConfederacaoBiz.java
@@ -1,5 +1,45 @@
 package com.treinamento.comitesquad.biz;
 
-public class ConfederacaoBiz {
+import com.treinamento.comitesquad.Mensagem;
+import com.treinamento.comitesquad.entities.Confedercao;
+import com.treinamento.comitesquad.repositories.ComiteRepository;
 
+public class ConfederacaoBiz {
+	private Mensagem mensagem;
+	private ComiteRepository comiteRepository;
+	
+	public ConfederacaoBiz(ComiteRepository comiteRepository) {
+		this.comiteRepository = comiteRepository;
+		this.mensagem = new Mensagem();
+	}
+
+	public Mensagem getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(Mensagem mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+	public Boolean validar(Confedercao confedercao) {
+		Boolean valido = true;
+		
+		if (confedercao.getNome().isEmpty()) {
+			getMensagem().mensagem.add("O nome não pode ser vazio");
+			valido = false;
+		}
+		if (confedercao.getNome().length() > 255) {
+			getMensagem().mensagem.add("O nome não pode ser maior que 255 caracteres!");
+			valido = false;
+		}
+		if (comiteRepository.findById(confedercao.getId()).isEmpty()) {
+			getMensagem().mensagem.add("Comitê não cadastrado");
+			valido = false;
+		}
+		if (confedercao.getId() == null) {
+			getMensagem().mensagem.add("O id comitê não deve ser nulo");
+			valido = false;
+		}
+		return valido;
+	}
 }
